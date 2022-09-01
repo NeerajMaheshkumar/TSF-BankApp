cb0a6d0164722145ca8e7043845b70efda8f725a
NeerajMaheshkumar <112578322+NeerajMaheshkumar@users.noreply.github.com>
2022-09-01 11:50:40 +0530

    Add files via upload

---
+++ UI-UX/src/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  
+  // Local
+            //endPoint: 'https://localhost:44393/api/',        
+            AuthPoint: 'https://localhost:8080/BankApp/',
+            //GenaralPoint: 'http://192.168.1.55:2002/api/',
+            //repEndpoint: 'http://localhost:10343/',
+};
